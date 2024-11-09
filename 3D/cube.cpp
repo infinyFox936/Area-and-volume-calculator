6481fb6bc8fbb8672cfffefac92eb42c2bf3a953
infinyFox936 <mezadragosh@gmail.com>
2024-11-09 18:16:16 +0200

    Uploaded cube.cpp

---
+++ 3D/cube.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter the length of the cube:" << endl;
+    cin >> x;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    //
+    cout << "The volume of the cube is:" << endl;
+    cout << pow(x, 3) << endl;
+    return 0;
+}
