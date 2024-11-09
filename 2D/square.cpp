182037384c8b5e1747ee99b4d445d279249df41a
infinyFox936 <mezadragosh@gmail.com>
2024-11-09 16:09:34 +0200

    Uploaded square.cpp

---
+++ 2D/square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x;
+	cout << "Enter the length of the square:" << endl;
+	cin >> x;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	//
+	cout << "The aria of the square is:" << endl;
+	cout << pow(x, 2);
+    return 0;
+}
